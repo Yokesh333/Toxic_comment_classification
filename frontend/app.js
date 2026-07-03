c881f1ed754a96f2aca0fe0307e05b8477926d21
Yokesh333 <yokeshd59@gmail.com>
2026-07-03 21:18:43 +0530

    updated

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 // Configuration
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = `${window.location.protocol}//${window.location.hostname}:8000`;
 
 // DOM Elements - Inputs
 const commentInput = document.getElementById("comment-input");
